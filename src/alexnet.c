bb33ac63286501e3a826b48c45c24a9c589c34d7
jackeyf <yangfei@umich.edu>
2025-03-12 11:55:16 -0700

    fix: inference arg bugs

--- src/alexnet.c
+++ src/alexnet.c
@@ -9,26 +9,25 @@
 #include <assert.h>
 #include "alexnet.h"
 
-
-void metrics(float *ret, int *preds, int *labels, 
-                int classes, int totNum, int type)
+void metrics(float *ret, int *preds, int *labels,
+             int classes, int totNum, int type)
 {
     /**
      * Compute metric on 'preds' and 'labels'
-     * 
+     *
      * Input:
      *      preds   [totNum]
      *      labels  [totNum]
-     *      classes 
+     *      classes
      *      totNum
-     *      type    
+     *      type
      * Output:
-     *      ret     
+     *      ret
      * */
 
-    int *totPred  = (int *)malloc(classes * sizeof(int)),
+    int *totPred = (int *)malloc(classes * sizeof(int)),
         *totLabel = (int *)malloc(classes * sizeof(int)),
-        *TP       = (int *)malloc(classes * sizeof(int));
+        *TP = (int *)malloc(classes * sizeof(int));
     memset(totPred, 0, classes * sizeof(int));
     memset(totLabel, 0, classes * sizeof(int));
     memset(TP, 0, classes * sizeof(int));
@@ -37,14 +36,14 @@ void metrics(float *ret, int *preds, int *labels,
     {
         totPred[preds[p]]++;
         totLabel[labels[p]]++;
-        if(preds[p] == labels[p])
+        if (preds[p] == labels[p])
         {
             TP[preds[p]]++;
         }
     }
 
-    int tmp_a=0, tmp_b=0;
-    for (int p =0 ; p < classes; p++)
+    int tmp_a = 0, tmp_b = 0;
+    for (int p = 0; p < classes; p++)
     {
         tmp_a += TP[p];
     }
@@ -97,7 +96,7 @@ void metrics(float *ret, int *preds, int *labels,
 
     if (type == METRIC_F1SCORE)
     {
-        *ret = 2*macro_p*macro_r / (macro_p+macro_r);
+        *ret = 2 * macro_p * macro_r / (macro_p + macro_r);
         free(totPred);
         free(totLabel);
         free(TP);
@@ -113,16 +112,16 @@ int argmax(float *arr, int n)
 {
     /**
      * Return the index of max-value among arr ~ arr+n
-     * 
+     *
      * Input:
      *      arr
      * Output:
      * Return:
      *      the index of max-value
-     * */ 
-    int   idx = -1;
+     * */
+    int idx = -1;
     float max = -1111111111;
-    for (int p = 0; p<n; p++)
+    for (int p = 0; p < n; p++)
     {
         if (arr[p] > max)
         {
@@ -130,18 +129,17 @@ int argmax(float *arr, int n)
             max = arr[p];
         }
     }
-    assert(idx!=-1);
+    assert(idx != -1);
     return idx;
 }
 
-
-static struct timespec start, finish; 
+static struct timespec start, finish;
 static float duration;
 void forward_alexnet(alexnet *net)
 {
     net->conv1.input = net->input;
 
-    //printf(">>>>>>>>>>>>>>>>>>conv1>>>>>>>>>>>>>>>>>>>>>>>>> \n");
+    // printf(">>>>>>>>>>>>>>>>>>conv1>>>>>>>>>>>>>>>>>>>>>>>>> \n");
 #ifdef SHOW_OP_TIME
     clock_gettime(CLOCK_MONOTONIC, &start);
 #endif
@@ -175,7 +173,7 @@ void forward_alexnet(alexnet *net)
     net->mp1.input = net->relu1.output;
     max_pooling_op_forward(&(net->mp1));
 
-    //printf(">>>>>>>>>>>>>>>>>>conv2>>>>>>>>>>>>>>>>>>>>>>>>> \n");
+    // printf(">>>>>>>>>>>>>>>>>>conv2>>>>>>>>>>>>>>>>>>>>>>>>> \n");
 #ifdef SHOW_OP_TIME
     clock_gettime(CLOCK_MONOTONIC, &start);
 #endif
@@ -213,7 +211,7 @@ void forward_alexnet(alexnet *net)
 #ifdef SHOW_OP_TIME
     clock_gettime(CLOCK_MONOTONIC, &start);
 #endif
-    //printf(">>>>>>>>>>>>>>>>>>conv3>>>>>>>>>>>>>>>>>>>>>>>>> \n");
+    // printf(">>>>>>>>>>>>>>>>>>conv3>>>>>>>>>>>>>>>>>>>>>>>>> \n");
     net->conv3.output = (float *)calloc(net->batchsize * net->conv3.out_units, sizeof(float));
     net->conv3.input = net->mp2.output;
     conv_op_forward(&(net->conv3));
@@ -241,7 +239,7 @@ void forward_alexnet(alexnet *net)
     net->relu3.input = net->bn3.output;
     relu_op_forward(&(net->relu3));
 
-    //printf(">>>>>>>>>>>>>>>>>>conv4>>>>>>>>>>>>>>>>>>>>>>>>> \n");
+    // printf(">>>>>>>>>>>>>>>>>>conv4>>>>>>>>>>>>>>>>>>>>>>>>> \n");
 #ifdef SHOW_OP_TIME
     clock_gettime(CLOCK_MONOTONIC, &start);
 #endif
@@ -272,7 +270,7 @@ void forward_alexnet(alexnet *net)
     net->relu4.input = net->bn4.output;
     relu_op_forward(&(net->relu4));
 
-    //printf(">>>>>>>>>>>>>>>>>>conv5>>>>>>>>>>>>>>>>>>>>>>>>> \n");
+    // printf(">>>>>>>>>>>>>>>>>>conv5>>>>>>>>>>>>>>>>>>>>>>>>> \n");
 #ifdef SHOW_OP_TIME
     clock_gettime(CLOCK_MONOTONIC, &start);
 #endif
@@ -306,7 +304,6 @@ void forward_alexnet(alexnet *net)
     net->mp5.output = (float *)malloc(net->batchsize * sizeof(float) * net->mp5.out_units);
     net->mp5.input = net->relu5.output;
     max_pooling_op_forward(&(net->mp5));
-
 
     dropout(net->mp5.output, DROPOUT_PROB, net->mp5.batchsize * net->mp5.out_units);
 
@@ -342,9 +339,9 @@ void forward_alexnet(alexnet *net)
     printf(" forward (&(net->fc2)) duration: %.4fs \n", duration);
 #endif
 
-    for(int p=0; p< net->fc2.out_units * net->batchsize; p++)
+    for (int p = 0; p < net->fc2.out_units * net->batchsize; p++)
     {
-        if(net->fc2.output[p]<(0-64) | net->fc2.output[p]>64)
+        if (net->fc2.output[p] < (0 - 64) | net->fc2.output[p] > 64)
         {
             printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!Forward: fc2 too big/small !!! %f \n", net->fc2.output[p]);
             break;
@@ -370,7 +367,6 @@ void forward_alexnet(alexnet *net)
 
     net->output = net->fc3.output;
 }
-
 
 void malloc_alexnet(alexnet *net)
 {
@@ -410,31 +406,35 @@ void free_alexnet(alexnet *net)
 
 static inline float U_Random(void)
 {
-    srand( (unsigned)time( NULL ) );
+    srand((unsigned)time(NULL));
     return (float)(rand() % 100) / 100;
 }
 
 static void gauss_initialization(float *p, int n, int in_units, int out_units)
 {
-    float mean  = 0;
+    float mean = 0;
     float stddv = 0.01;
 
-	float V1, V2, S, X;
-	static int phase = 0;
+    float V1, V2, S, X;
+    static int phase = 0;
     for (int shift = 0; shift < n; shift++)
     {
-        if (phase == 0) {
-            do {
-                float U1 = (float) rand() / RAND_MAX;
-                float U2 = (float) rand() / RAND_MAX;
+        if (phase == 0)
+        {
+            do
+            {
+                float U1 = (float)rand() / RAND_MAX;
+                float U2 = (float)rand() / RAND_MAX;
 
                 V1 = 2 * U1 - 1;
                 V2 = 2 * U2 - 1;
                 S = V1 * V1 + V2 * V2;
             } while (S >= 1 || S == 0);
-    
+
             X = V1 * sqrt(-2 * log(S) / S);
-        }else {
+        }
+        else
+        {
             X = V2 * sqrt(-2 * log(S) / S);
         }
         phase = 1 - phase;
@@ -442,7 +442,6 @@ static void gauss_initialization(float *p, int n, int in_units, int out_units)
         p[shift] = mean + stddv * X;
     }
 }
-
 
 //
 // save trainable weights of network
@@ -496,7 +495,6 @@ void load_alexnet(alexnet *net, char *filename)
     printf("Load weights from \"%s\" successfully... \n", filename);
 }
 
-
 void setup_alexnet(alexnet *net, short batchsize)
 {
     /**
@@ -508,17 +506,17 @@ void setup_alexnet(alexnet *net, short batchsize)
     net->conv3.batchsize = batchsize;
     net->conv4.batchsize = batchsize;
     net->conv5.batchsize = batchsize;
-    net->fc1.batchsize   = batchsize;
-    net->fc2.batchsize   = batchsize;
-    net->fc3.batchsize   = batchsize;
-    net->bn1.batchsize   = batchsize;
-    net->bn2.batchsize   = batchsize;
-    net->bn3.batchsize   = batchsize;
-    net->bn4.batchsize   = batchsize;
-    net->bn5.batchsize   = batchsize;
-    net->mp1.batchsize   = batchsize;
-    net->mp2.batchsize   = batchsize;
-    net->mp5.batchsize   = batchsize;
+    net->fc1.batchsize = batchsize;
+    net->fc2.batchsize = batchsize;
+    net->fc3.batchsize = batchsize;
+    net->bn1.batchsize = batchsize;
+    net->bn2.batchsize = batchsize;
+    net->bn3.batchsize = batchsize;
+    net->bn4.batchsize = batchsize;
+    net->bn5.batchsize = batchsize;
+    net->mp1.batchsize = batchsize;
+    net->mp2.batchsize = batchsize;
+    net->mp5.batchsize = batchsize;
     net->relu1.batchsize = batchsize;
     net->relu2.batchsize = batchsize;
     net->relu3.batchsize = batchsize;
@@ -536,13 +534,13 @@ void setup_alexnet(alexnet *net, short batchsize)
     net->conv1.stride = C1_STRIDES;
     net->conv1.out_h = FEATURE1_L;
     net->conv1.out_w = FEATURE1_L;
-    net->conv1.in_units = IN_CHANNELS*FEATURE0_L*FEATURE0_L;
-    net->conv1.out_units = C1_CHANNELS*FEATURE1_L*FEATURE1_L;
+    net->conv1.in_units = IN_CHANNELS * FEATURE0_L * FEATURE0_L;
+    net->conv1.out_units = C1_CHANNELS * FEATURE1_L * FEATURE1_L;
 
     net->bn1.units = net->conv1.out_units;
 
     net->relu1.units = net->bn1.units;
-    
+
     net->mp1.channels = C1_CHANNELS;
     net->mp1.stride = 2;
     net->mp1.kernel_size = 3;
@@ -551,7 +549,7 @@ void setup_alexnet(alexnet *net, short batchsize)
     net->mp1.out_w = POOLING1_L;
     net->mp1.out_h = POOLING1_L;
     net->mp1.in_units = net->relu1.units;
-    net->mp1.out_units = C1_CHANNELS*POOLING1_L*POOLING1_L;
+    net->mp1.out_units = C1_CHANNELS * POOLING1_L * POOLING1_L;
 
     net->conv2.in_channels = C1_CHANNELS;
     net->conv2.out_channels = C2_CHANNELS;
@@ -563,7 +561,7 @@ void setup_alexnet(alexnet *net, short batchsize)
     net->conv2.out_h = FEATURE2_L;
     net->conv2.out_w = FEATURE2_L;
     net->conv2.in_units = net->mp1.out_units;
-    net->conv2.out_units = C2_CHANNELS*FEATURE2_L*FEATURE2_L;
+    net->conv2.out_units = C2_CHANNELS * FEATURE2_L * FEATURE2_L;
 
     net->bn2.units = net->conv2.out_units;
 
@@ -577,7 +575,7 @@ void setup_alexnet(alexnet *net, short batchsize)
     net->mp2.out_w = POOLING2_L;
     net->mp2.out_h = POOLING2_L;
     net->mp2.in_units = net->relu2.units;
-    net->mp2.out_units = C2_CHANNELS*POOLING2_L*POOLING2_L;
+    net->mp2.out_units = C2_CHANNELS * POOLING2_L * POOLING2_L;
 
     net->conv3.in_channels = C2_CHANNELS;
     net->conv3.out_channels = C3_CHANNELS;
@@ -589,7 +587,7 @@ void setup_alexnet(alexnet *net, short batchsize)
     net->conv3.out_h = FEATURE3_L;
     net->conv3.out_w = FEATURE3_L;
     net->conv3.in_units = net->mp2.out_units;
-    net->conv3.out_units = C3_CHANNELS*FEATURE3_L*FEATURE3_L;
+    net->conv3.out_units = C3_CHANNELS * FEATURE3_L * FEATURE3_L;
 
     net->bn3.units = net->conv3.out_units;
 
@@ -605,7 +603,7 @@ void setup_alexnet(alexnet *net, short batchsize)
     net->conv4.out_h = FEATURE4_L;
     net->conv4.out_w = FEATURE4_L;
     net->conv4.in_units = net->relu3.units;
-    net->conv4.out_units = C4_CHANNELS*FEATURE4_L*FEATURE4_L;
+    net->conv4.out_units = C4_CHANNELS * FEATURE4_L * FEATURE4_L;
 
     net->bn4.units = net->conv4.out_units;
 
@@ -621,7 +619,7 @@ void setup_alexnet(alexnet *net, short batchsize)
     net->conv5.out_h = FEATURE5_L;
     net->conv5.out_w = FEATURE5_L;
     net->conv5.in_units = net->relu4.units;
-    net->conv5.out_units = C5_CHANNELS*FEATURE5_L*FEATURE5_L;
+    net->conv5.out_units = C5_CHANNELS * FEATURE5_L * FEATURE5_L;
 
     net->bn5.units = net->conv5.out_units;
 
@@ -635,13 +633,13 @@ void setup_alexnet(alexnet *net, short batchsize)
     net->mp5.out_w = POOLING5_L;
     net->mp5.out_h = POOLING5_L;
     net->mp5.in_units = net->relu5.units;
-    net->mp5.out_units = C5_CHANNELS*POOLING5_L*POOLING5_L;
+    net->mp5.out_units = C5_CHANNELS * POOLING5_L * POOLING5_L;
 
     net->fc1.in_units = net->mp5.out_units;
     net->fc1.out_units = FC6_LAYER;
-    
-    net->relu6.units = FC6_LAYER; 
-    
+
+    net->relu6.units = FC6_LAYER;
+
     net->fc2.in_units = FC6_LAYER;
     net->fc2.out_units = FC7_LAYER;
 
@@ -654,132 +652,136 @@ void setup_alexnet(alexnet *net, short batchsize)
 void alexnet_init_weights(alexnet *net, char *weights_path)
 {
 
-    if(weights_path != NULL) // load a pre-trained model
+    if (weights_path != NULL) // load a pre-trained model
     {
         load_alexnet(net, weights_path);
         return;
     }
 
     // initialize weights for this network
-    gauss_initialization(net->conv1.weights, C1_CHANNELS*IN_CHANNELS*C1_KERNEL_L*C1_KERNEL_L, net->conv1.in_units, net->conv1.out_units);
-    gauss_initialization(net->conv2.weights, C2_CHANNELS*C1_CHANNELS*C2_KERNEL_L*C2_KERNEL_L, net->conv2.in_units, net->conv2.out_units);
-    gauss_initialization(net->conv3.weights, C3_CHANNELS*C2_CHANNELS*C3_KERNEL_L*C3_KERNEL_L, net->conv3.in_units, net->conv3.out_units);
-    gauss_initialization(net->conv4.weights, C4_CHANNELS*C3_CHANNELS*C4_KERNEL_L*C4_KERNEL_L, net->conv4.in_units, net->conv4.out_units);
-    gauss_initialization(net->conv5.weights, C5_CHANNELS*C4_CHANNELS*C5_KERNEL_L*C5_KERNEL_L, net->conv5.in_units, net->conv5.out_units);
-    gauss_initialization(net->fc1.weights, C5_CHANNELS*FC6_LAYER*POOLING5_L*POOLING5_L, net->fc1.in_units, net->fc1.out_units);
-    gauss_initialization(net->fc2.weights, FC6_LAYER*FC7_LAYER, net->fc2.in_units, net->fc2.out_units);
-    gauss_initialization(net->fc3.weights, FC7_LAYER*OUT_LAYER, net->fc3.in_units, net->fc3.out_units);
+    gauss_initialization(net->conv1.weights, C1_CHANNELS * IN_CHANNELS * C1_KERNEL_L * C1_KERNEL_L, net->conv1.in_units, net->conv1.out_units);
+    gauss_initialization(net->conv2.weights, C2_CHANNELS * C1_CHANNELS * C2_KERNEL_L * C2_KERNEL_L, net->conv2.in_units, net->conv2.out_units);
+    gauss_initialization(net->conv3.weights, C3_CHANNELS * C2_CHANNELS * C3_KERNEL_L * C3_KERNEL_L, net->conv3.in_units, net->conv3.out_units);
+    gauss_initialization(net->conv4.weights, C4_CHANNELS * C3_CHANNELS * C4_KERNEL_L * C4_KERNEL_L, net->conv4.in_units, net->conv4.out_units);
+    gauss_initialization(net->conv5.weights, C5_CHANNELS * C4_CHANNELS * C5_KERNEL_L * C5_KERNEL_L, net->conv5.in_units, net->conv5.out_units);
+    gauss_initialization(net->fc1.weights, C5_CHANNELS * FC6_LAYER * POOLING5_L * POOLING5_L, net->fc1.in_units, net->fc1.out_units);
+    gauss_initialization(net->fc2.weights, FC6_LAYER * FC7_LAYER, net->fc2.in_units, net->fc2.out_units);
+    gauss_initialization(net->fc3.weights, FC7_LAYER * OUT_LAYER, net->fc3.in_units, net->fc3.out_units);
 
     int i;
-    for(i=0; i<C1_CHANNELS; i++)
+    for (i = 0; i < C1_CHANNELS; i++)
         net->conv1.bias[i] = 1;
-    for(i=0; i<C2_CHANNELS; i++)
+    for (i = 0; i < C2_CHANNELS; i++)
         net->conv2.bias[i] = 1;
-    for(i=0; i<C3_CHANNELS; i++)
+    for (i = 0; i < C3_CHANNELS; i++)
         net->conv3.bias[i] = 1;
-    for(i=0; i<C4_CHANNELS; i++)
+    for (i = 0; i < C4_CHANNELS; i++)
         net->conv4.bias[i] = 1;
-    for(i=0; i<C5_CHANNELS; i++)
+    for (i = 0; i < C5_CHANNELS; i++)
         net->conv5.bias[i] = 1;
-    for(i=0; i<FC6_LAYER; i++)
+    for (i = 0; i < FC6_LAYER; i++)
         net->fc1.bias[i] = 1;
-    for(i=0; i<FC7_LAYER; i++)
+    for (i = 0; i < FC7_LAYER; i++)
         net->fc2.bias[i] = 1;
-    for(i=0; i<OUT_LAYER; i++)
+    for (i = 0; i < OUT_LAYER; i++)
         net->fc3.bias[i] = 1;
 
-    for(i=0; i<(net->bn1.units); i++)
+    for (i = 0; i < (net->bn1.units); i++)
         net->bn1.gamma[i] = 1;
-    for(i=0; i<(net->bn2.units); i++)
+    for (i = 0; i < (net->bn2.units); i++)
         net->bn2.gamma[i] = 1;
-    for(i=0; i<(net->bn3.units); i++)
+    for (i = 0; i < (net->bn3.units); i++)
         net->bn3.gamma[i] = 1;
-    for(i=0; i<(net->bn4.units); i++)
+    for (i = 0; i < (net->bn4.units); i++)
         net->bn4.gamma[i] = 1;
-    for(i=0; i<(net->bn5.units); i++)
+    for (i = 0; i < (net->bn5.units); i++)
         net->bn5.gamma[i] = 1;
-    
-    memset(net->bn1.beta, 0, sizeof(float)*(net->bn1.units));
-    memset(net->bn2.beta, 0, sizeof(float)*(net->bn2.units));
-    memset(net->bn3.beta, 0, sizeof(float)*(net->bn3.units));
-    memset(net->bn4.beta, 0, sizeof(float)*(net->bn4.units));
-    memset(net->bn5.beta, 0, sizeof(float)*(net->bn5.units));
+
+    memset(net->bn1.beta, 0, sizeof(float) * (net->bn1.units));
+    memset(net->bn2.beta, 0, sizeof(float) * (net->bn2.units));
+    memset(net->bn3.beta, 0, sizeof(float) * (net->bn3.units));
+    memset(net->bn4.beta, 0, sizeof(float) * (net->bn4.units));
+    memset(net->bn5.beta, 0, sizeof(float) * (net->bn5.units));
 }
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     /**
-     * 
+     *
      * Entrance
-     * 
+     *
      * */
     static alexnet net;
-    
+
     if (0 == strcmp(argv[1], "train"))
     {
-        // 
+        //
         // $./alexnet train -batchsize 4 -epochs 1000 -load_pretrained ./in.weights -save ./out.weights
         //
-        int     batchsize = 8, 
-                epochs = 1000;
-        short   is_save=0, 
-                is_load=0;
-        char    weights_in_path[256];
-        char    weights_out_path[256]; 
-        for (int i = 2; i < argc-1; i++)
+        int batchsize = 8,
+            epochs = 1000;
+        short is_save = 0,
+              is_load = 0;
+        char weights_in_path[256];
+        char weights_out_path[256];
+        for (int i = 2; i < argc - 1; i++)
         {
             if (0 == strcmp(argv[i], "-batchsize"))
-                sscanf(argv[i+1], "%d", &batchsize);
+                sscanf(argv[i + 1], "%d", &batchsize);
             if (0 == strcmp(argv[i], "-epochs"))
-                sscanf(argv[i+1], "%d", &epochs);
+                sscanf(argv[i + 1], "%d", &epochs);
             if (0 == strcmp(argv[i], "-load_pretrained"))
             {
-                is_load=1;
-                sprintf(weights_in_path, "%s", argv[i+1]);
+                is_load = 1;
+                sprintf(weights_in_path, "%s", argv[i + 1]);
             }
             if (0 == strcmp(argv[i], "-save"))
             {
-                is_save=1;
-                sprintf(weights_out_path, "%s", argv[i+1]);
+                is_save = 1;
+                sprintf(weights_out_path, "%s", argv[i + 1]);
             }
         }
 
         printf("batch size: %d \n", batchsize);
         printf("epochs: %d \n", epochs);
         setup_alexnet(&net, batchsize);
-        malloc_alexnet(&net);    
-        if (is_load) {
+        malloc_alexnet(&net);
+        if (is_load)
+        {
             alexnet_init_weights(&net, weights_in_path);
-        }else {
+        }
+        else
+        {
             alexnet_init_weights(&net, NULL);
         }
         alexnet_train(&net, epochs);
         if (is_save)
             save_alexnet(&net, weights_out_path);
         free_alexnet(&net);
-
-    }else if (0 == strcmp(argv[1], "inference"))
+    }
+    else if (0 == strcmp(argv[1], "inference"))
     {
         //
         // $./alexnet inference -input ./data/0001.jpeg -load ./a1.weights
         //
         char img_path[256];
         char weights_path[256];
-        for (int i = 2; i < argc-1; i++)
+        for (int i = 2; i < argc - 1; i++)
         {
             if (0 == strcmp(argv[i], "-input"))
-                sprintf(img_path, "%s", argv[i+1]);
+                sprintf(img_path, "%s", argv[i + 1]);
             if (0 == strcmp(argv[i], "-load"))
-                sprintf(weights_path, "%s", argv[i+1]);
+                sprintf(weights_path, "%s", argv[i + 1]);
         }
         setup_alexnet(&net, 1);
-        malloc_alexnet(&net);    
+        malloc_alexnet(&net);
         alexnet_init_weights(&net, weights_path);
         printf("alexnet setup fininshed. Waiting for inference...\n");
-        alexnet_inference(&net, argv[2]);
+        alexnet_inference(&net, img_path);
         free_alexnet(&net);
-    }else {
+    }
+    else
+    {
         printf("Error: Unknown argument!!! \n");
     }
 }
